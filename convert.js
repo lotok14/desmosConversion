fb06b12d8b76dbd86438d7406aea1f7db4162dde
lotok14 <98856437+lotok14@users.noreply.github.com>
2024-02-14 23:00:51 +0100

    Create convert.js

---
+++ convert.js
@@ -0,0 +1,40 @@
+function copyToClipboard(message) {
+      navigator.clipboard.writeText(message).then(
+        () => { },
+        () => {
+          alert("Couldn't copy text to clipboard!");
+        }
+      );
+    }
+
+    function handleCopyToClipboardClick() {
+      toCopy = document.getElementById('output').innerHTML;
+      copyToClipboard(toCopy);
+    }
+
+    function handleConvertClick() {
+      var lines = input.value;
+      lines = lines.split("\n");
+
+      for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+
+        line = line.split(" ");
+        line = line.filter(e => e);
+        console.log(line);
+        line = line.join("\t");
+        console.log(line);
+        lines[i] = line;
+      }
+      lines = lines.join("\n");
+      output.value = lines;
+
+    }
+
+    const input = document.getElementById("input");
+    const output = document.getElementById("output");
+    const copy_to_clipboard_button = document.getElementById("copy_to_clipboard_button");
+    const convert = document.getElementById("convert");
+
+    convert.addEventListener('click', handleConvertClick);
+    copy_to_clipboard_button.addEventListener('click', handleCopyToClipboardClick);
